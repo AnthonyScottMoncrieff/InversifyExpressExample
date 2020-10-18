66749a0fdbb37a86d730f2e81c3af7dbeeb6bd91
AnthonyScottMoncrieff <anthony.scottmoncrieff@gmail.com>
2020-10-18 17:40:30 +0100

    Fixed _id constraints

--- src/InversifyExpressExample.Data/DbClient.ts
+++ src/InversifyExpressExample.Data/DbClient.ts
@@ -14,23 +14,23 @@ export class DbClient implements IDbClient {
         });
     }
 
-    public async FindAsync<T extends {_id?:string}>(collection: string, filter: Object): Promise<T[]> {
+    public async FindAsync<T extends {_id:string}>(collection: string, filter: Object): Promise<T[]> {
         let find: T[] = await this._db.collection(collection).find(filter).toArray();
         return find;
     }
 
-    public async FindOneByIdAsync<T extends {_id?:string}>(collection: string, objectId: string): Promise<T> {
+    public async FindOneByIdAsync<T extends {_id:string}>(collection: string, objectId: string): Promise<T> {
         let find = await this._db.collection(collection).find({ _id: new ObjectID(objectId) }).limit(1).toArray();
         let first : T = find[0];
         return first;
     }
 
-    public async InsertAsync<T extends {_id?:string}>(collection: string, model: T): Promise<T> {
+    public async InsertAsync<T extends {_id:string}>(collection: string, model: T): Promise<T> {
         let insert = await this._db.collection(collection).insertOne(model);
         return <T>insert.ops[0];
     }
 
-    public async UpdateAsync<T extends {_id?:string}>(collection: string, objectId: string, model: T): Promise<void> {
+    public async UpdateAsync<T extends {_id:string}>(collection: string, objectId: string, model: T): Promise<void> {
         await this._db.collection(collection).updateOne({ _id: new ObjectID(objectId) }, { $set: model });
     }
 
--- src/InversifyExpressExample.Data/Interfaces/IDbClient.ts
+++ src/InversifyExpressExample.Data/Interfaces/IDbClient.ts
@@ -1,7 +1,7 @@
 export interface IDbClient{
-    FindAsync<T>(collection: string, filter: Object): Promise<T[]>;
-    FindOneByIdAsync<T>(collection: string, objectId: string): Promise<T>;
-    InsertAsync<T extends {_id:any}>(collection: string, model: T): Promise<T>;
-    UpdateAsync<T>(collection: string, objectId: string, model: T): Promise<void>;
+    FindAsync<T extends {_id:string}>(collection: string, filter: Object): Promise<T[]>;
+    FindOneByIdAsync<T extends {_id:string}>(collection: string, objectId: string): Promise<T>;
+    InsertAsync<T extends {_id:string}>(collection: string, model: T): Promise<T>;
+    UpdateAsync<T extends {_id:string}>(collection: string, objectId: string, model: T): Promise<void>;
     RemoveAsync(collection: string, objectId: string): Promise<void>;
 }
